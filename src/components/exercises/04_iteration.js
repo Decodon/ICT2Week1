60fa88b8d00cb42bf4796828880386dc5e7f4621
Decodon <20095352@mail.wit.ie>
2022-07-08 17:33:23 +0100

    Exercise 4 started.

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+const Demo = (props) => {
+  const list = props.modules.map((module, index) => (
+    <tr key={index}>
+      {module.name}
+      {module.noLectures}
+      {module.noPracticals}
+    </tr>
+  ));
 
-const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <>
+      <h2>{props.course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    </>
   );
 };
 
